fb0e092da0aa2c5f1ec63b656ae5b473f34bd552
marcus6183 <marcusw6183@gmail.com>
2023-04-03 16:50:40 +0800

    Added Sub menu for About Us
    Split LeanTweenScript into TweenLogo, TweenBtnGroup and TweenBtnSlide
    
    Added SubMenuAboutUs to build settings
    
    Linked About Us button to SubMenuAboutUs in Main Menu

--- Assets/Scripts/TweenBtnGroup.cs
+++ Assets/Scripts/TweenBtnGroup.cs
@@ -2,15 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LeanTweenScript : MonoBehaviour
+public class TweenBtnGroup : MonoBehaviour
 {
-    [SerializeField] GameObject sainsLogo, buttonGroup;
-    public float posY;
+    [SerializeField] GameObject buttonGroup;
     public float animateDuration;
+
     // Start is called before the first frame update
     void Start()
     {
-        LeanTween.moveY(sainsLogo, posY, animateDuration).setEase(LeanTweenType.easeOutCubic);
         LeanTween.moveY(buttonGroup, -500f, animateDuration).setEase(LeanTweenType.easeOutCubic);
     }
 
